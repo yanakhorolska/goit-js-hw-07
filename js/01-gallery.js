d17969c9e07a260f64096b2c793d6d97a5cb67da
yanakhorolska <yana.khorolska@protonmail.com>
2022-08-13 18:58:29 +0200

    update sliders

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryItem = galleryItems.map((item) =>
+    `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></div>`).join(' ');
+
+gallery.innerHTML = galleryItem;
+
+gallery.addEventListener('click', handleImageClick);
+
+function handleImageClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+    
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+    instance.show();
+    
+    gallery.addEventListener('keydown', (evt) => {
+        if (evt.code === "Escape") {
+            instance.close();
+        }
+    });
+}
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryItem = galleryItems.map((item) =>
+    `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a>`)
+    .join("");
+
+gallery.innerHTML = galleryItem;
+
+// gallery.addEventListener('click', handlerImage);
+
+
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 1,
+    captionsData: "alt",
+    captionDelay: 250,
+    });
+
+
+
